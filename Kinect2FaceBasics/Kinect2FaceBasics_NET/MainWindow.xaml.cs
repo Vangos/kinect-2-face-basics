190fba681e7ff31ef81f992f7d73c4a008ec2556
Vangos <pterneas@gmail.com>
2014-12-13 05:28:45 +0200

    Added the initial .NET project

---
+++ Kinect2FaceBasics/Kinect2FaceBasics_NET/MainWindow.xaml.cs
@@ -0,0 +1,138 @@
+﻿using Microsoft.Kinect;
+using Microsoft.Kinect.Face;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Kinect2FaceBasics_NET
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        KinectSensor _sensor = null;
+        ColorFrameReader _colorReader = null;
+        BodyFrameReader _bodyReader = null;
+        IList<Body> _bodies = null;
+
+        // 1) Specify a face frame source and a face frame reader
+        FaceFrameSource _faceSource = null;
+        FaceFrameReader _faceReader = null;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            _sensor = KinectSensor.GetDefault();
+
+            if (_sensor != null)
+            {
+                _sensor.Open();
+
+                _bodies = new Body[_sensor.BodyFrameSource.BodyCount];
+
+                _colorReader = _sensor.ColorFrameSource.OpenReader();
+                _colorReader.FrameArrived += ColorReader_FrameArrived;
+                _bodyReader = _sensor.BodyFrameSource.OpenReader();
+                _bodyReader.FrameArrived += BodyReader_FrameArrived;
+
+                // 2) Initialize the face source with the desired features
+                _faceSource = new FaceFrameSource(_sensor, 0, FaceFrameFeatures.BoundingBoxInColorSpace |
+                                                              FaceFrameFeatures.FaceEngagement |
+                                                              FaceFrameFeatures.Glasses |
+                                                              FaceFrameFeatures.Happy |
+                                                              FaceFrameFeatures.LeftEyeClosed |
+                                                              FaceFrameFeatures.MouthOpen |
+                                                              FaceFrameFeatures.PointsInColorSpace |
+                                                              FaceFrameFeatures.RightEyeClosed);
+                _faceReader = _faceSource.OpenReader();
+                _faceReader.FrameArrived += FaceReader_FrameArrived;
+            }
+        }
+
+        void ColorReader_FrameArrived(object sender, ColorFrameArrivedEventArgs e)
+        {
+        }
+
+        void BodyReader_FrameArrived(object sender, BodyFrameArrivedEventArgs e)
+        {
+            using (var frame = e.FrameReference.AcquireFrame())
+            {
+                if (frame != null)
+                {
+                    frame.GetAndRefreshBodyData(_bodies);
+
+                    Body body = _bodies.Where(b => b.IsTracked).FirstOrDefault();
+
+                    if (!_faceSource.IsTrackingIdValid)
+                    {
+                        if (body != null)
+                        {
+                            // 4) Assign a tracking ID to the face source
+                            _faceSource.TrackingId = body.TrackingId;
+                        }
+                    }
+                }
+            }
+        }
+
+        void FaceReader_FrameArrived(object sender, FaceFrameArrivedEventArgs e)
+        {
+            using (var frame = e.FrameReference.AcquireFrame())
+            {
+                if (frame != null)
+                {
+                    // 4) Get the face frame result
+                    FaceFrameResult result = frame.FaceFrameResult;
+                    
+                    // 5) Do magic!
+                    // ...
+                }
+            }
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (_colorReader != null)
+            {
+                _colorReader.Dispose();
+                _colorReader = null;
+            }
+
+            if (_bodyReader != null)
+            {
+                _bodyReader.Dispose();
+                _bodyReader = null;
+            }
+
+            if (_faceReader != null)
+            {
+                _faceReader.Dispose();
+                _faceReader = null;
+            }
+
+            if (_faceSource != null)
+            {
+                _faceSource.Dispose();
+                _faceSource = null;
+            }
+
+            if (_sensor != null)
+            {
+                _sensor.Close();
+            }
+        }
+    }
+}
